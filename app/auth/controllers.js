7c75b694abbf14192d96b86e3dc01bbf820180a9
t1sev4 <tamikofficial@gmail.com>
2023-06-29 19:47:17 +0600

    add sign in user

--- app/auth/controllers.js
+++ app/auth/controllers.js
@@ -2,6 +2,8 @@ const sendMail = require('../utils/sendMail');
 const AuthCode = require('./AuthCode');
 const User = require('./User');
 const Role = require('./Role');
+const jwt = require('jsonwebtoken');
+const {jwtOptions} = require('./passport');
 
 const sendVerificationEmail = (req, res) => {
   const code = 'HH' + Date.now();
@@ -20,24 +22,49 @@ const sendVerificationEmail = (req, res) => {
 }
 
 const verifyCode = async (req, res) => {
-  console.log(req.body);
-  const authCode = await AuthCode.findOne({where: {email: req.body.email}});
+
+  const authCode = await AuthCode.findOne({
+    where: {email: req.body.email}, 
+    order: [['valid_till', 'DESC']]
+  });
+
   if(!authCode){
-    res.status(401).send({message: "code is invalid"});
+    res.status(401).send({message: "email not found"});
   }
   else if(new Date(authCode.valid_till).getTime() < Date.now()){
-    
-    res.status(401).send({message: "code is invalid"});
+    res.status(401).send({message: "code истек"});
   }else if(authCode.code !== req.body.code){
     res.status(401).send({message: "code is invalid"});
   }
   else{
+    
+    let user = await User.findOne({where: {email: req.body.email}}) 
     const role = await Role.findOne({where: {name: 'employee'}});
-    const user = await User.create({
-      RoleId: role.id,
-      email: req.body.email,
-    })
-    res.status(200).send(user);
+  
+    if(!user){
+      user = await User.create({
+        RoleId: role.id,
+        email: req.body.email,
+      })
+    }
+    
+    const token = jwt.sign({
+        id: user.id, 
+        email: user.email,
+        full_name: user.full_name,
+        phone: user.phone,
+        role: {
+          id: role.id,
+          name: role.name
+        }
+
+      }, 
+        jwtOptions.secretOrKey, 
+      {
+        expiresIn: 24 * 60 * 60 * 365
+      }
+    );
+    res.status(200).send({token});
   }  
 }
 
