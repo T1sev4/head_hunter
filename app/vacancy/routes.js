5473fcaa001a63cf6f8e20aca46747fbd035a218
t1sev4 <tamikofficial@gmail.com>
2023-08-15 16:08:03 +0600

    lesson 11 part 4 create model Experiences and seed for it, also router for get all Experiences

---
+++ app/vacancy/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const {getExperiences} = require('./controllers');
+router.get('/api/experiences', getExperiences); 
+
+
+
+module.exports = router;
