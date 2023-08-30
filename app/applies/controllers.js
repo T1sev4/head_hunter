2f1782d570b989c87b5a5eb85ed72618515e5082
t1sev4 <tamikofficial@gmail.com>
2023-08-30 22:19:54 +0600

    lesson 14 part 4, send message witn new apply to manager

--- app/applies/controllers.js
+++ app/applies/controllers.js
@@ -1,6 +1,9 @@
 const Apply = require('./Apply');
 const {NEW} = require('./utils')
-
+const sendMail = require('../utils/sendMail')
+const Vacancy = require('../vacancy/models/Vacancy')
+const Resume = require('../resume/models/Resume')
+const User = require('../auth/User');
 const createApply = async (req, res) => {
   try {
     const apply = await Apply.create({
@@ -9,6 +12,17 @@ const createApply = async (req, res) => {
       status: NEW,
     })
   
+    const resume = await Resume.findByPk(req.body.resumeId)
+    const vacancy = await Vacancy.findByPk(req.body.vacancyId)
+    const user = await User.findByPk(vacancy.userId);
+    sendMail(user.email, `Новый отклик на вакансию ${vacancy.name}`, 
+    `
+    Имя соискателя: ${resume.first_name}
+    Фамилия соискателя: ${resume.last_name}
+    Номер соискателя: ${resume.phone}
+    `
+    )
+
     res.status(200).send(apply);
   } catch (error) {
     res.status(500).send(error);
