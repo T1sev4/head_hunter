0b07bf88112f52bef123b25a165a72bd99b73b39
t1sev4 <tamikofficial@gmail.com>
2023-09-10 20:33:00 +0600

    fixed connection to db

--- config/config.js
+++ config/config.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const path = require("path")
+
+
 module.exports = {
   development:{
     username: 'admin',
@@ -12,6 +16,11 @@ module.exports = {
     database: 'defaultdb',
     host: 'db-postgresql-sgp1-79804-do-user-14638949-0.b.db.ondigitalocean.com',
     dialect: 'postgres',
-    port: 25060
+    port: 25060,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.resolve("config", "ca-certificate.crt")),
+      },
+    }
   },
 }
